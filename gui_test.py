029130901a42091ec91fea89330193597bf5b6f5
Jake Gillow <jake.gillow@gmail.com>
2022-08-31 21:08:18 -0900

    Playing with pi sense board

--- gui_test.py
+++ gui_test.py
@@ -1,4 +1,5 @@
 from guizero import App, Text
+from sense_hat import SenseHat
 
 app = App(title="Home Nexus")
 top_message = Text(app,"Home Interface",color="blue")
---
+++ sense_hat_test.py
@@ -0,0 +1,22 @@
+from asyncio import get_event_loop
+from sense_hat import SenseHat
+
+sense = SenseHat()
+#count=0
+#while (count<1):
+
+   #count =count +1
+   # sense.show_message("Fire Up Chips!",text_colour=(106,0,50),back_colour=(255,200,46))
+
+while True:
+    for x in sense.stick.get_events():
+        if x.direction == 'up':
+            sense.show_message("Hello World!",text_colour=(106,0,50),back_colour=(255,200,46))
+        elif x.direction == 'down':
+            sense.show_letter("D")
+        elif x.direction == 'left':
+            sense.show_letter("L")
+        elif x.direction == 'right':
+            sense.show_letter("R")
+        elif x.direction == 'middle':
+            sense.show_letter("M")
