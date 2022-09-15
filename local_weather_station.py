297b76c6c91783461302a400deb7675ef10bac3d
Jake Gillow <jake.gillow@gmail.com>
2022-09-14 17:57:04 -0900

    updated gui to have a grid pattern, added a close window button

--- local_weather_station.py
+++ local_weather_station.py
@@ -1,11 +1,18 @@
-from guizero import App, Text
+from os import close
+from tkinter import Button, Grid
+from guizero import App, Text, PushButton, Box
 from sense_hat import SenseHat
+import time
+import os
+import sys
 
 sense = SenseHat()
 
-app = App(title="Home Nexus")
+app = App(title="Home Nexus", height = 1000, width = 1000, layout="grid")
 
-top_message = Text(app,"Home Interface",color="blue")
+#box = Box(app, layout="grid", grid=[1,0])
+
+top_message = Text(app,"Home Interface",color="blue", grid=[0,0])
 
 temp_humidity = sense.get_temperature_from_humidity()
 temp_pressure = sense.get_temperature_from_pressure()
@@ -16,10 +23,11 @@ rounded_temp_humidity = round(temp_humidity, 2)
 
 #temp_humidity_title = "Temerature from Humidity"
 
-text_0 = Text(app, "Temperature (deg C)", color="blue",align="left")
-text_1 = Text(app,  rounded_temp_humidity, color="blue",align="left") 
-text_2 = Text(app, temp_humidity)
+text_0 = Text(app, "Temperature (deg C)", color="blue", grid=[1,0])
+text_1 = Text(app,  rounded_temp_humidity, color="blue", grid=[2,0])
+text_2 = Text(app, temp_humidity, grid=[3,0])
 
+Button = PushButton(app, text="Close Window", command=sys.exit, grid=[2,2])
 
 
 app.display()
